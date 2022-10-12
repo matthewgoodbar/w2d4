18105f435f2c9a80eef1f20b0f97e7377c677182
matthew <matthewgoodbar@gmail.com>
2022-10-12 12:07:19 -0700

    Implemented methods for recap 1

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -6,7 +6,16 @@
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
 def all_vowel_pairs(words)
-
+    pairs = []
+    (1...words.length).each do |outer_i|
+        (0...outer_i).each do |inner_i|
+            vowels="aeiou"
+            words[outer_i].each_char {|char| vowels.delete!(char) if vowels.include?(char)}
+            words[inner_i].each_char {|char| vowels.delete!(char) if vowels.include?(char)}
+            pairs.push("#{words[inner_i]} #{words[outer_i]}") if vowels == ""
+        end
+    end
+    return pairs
 end
 
 
@@ -18,7 +27,7 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
-
+    return (2..num/2).any? {|x| num % x == 0}
 end
 
 
@@ -32,7 +41,13 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+    res = []
+    bigrams.each do |bigram|
+        (0...(str.length-1)).each do |i|
+            res << bigram if str[i] == bigram[0] && str[i+1] == bigram[1]
+        end
+    end
+    return res
 end
 
 class Hash
@@ -50,7 +65,10 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+        prc ||= Proc.new {|k,v| k == v}
+        hsh = {}
+        self.each {|k,v| hsh[k] = v if prc.call(k,v)}
+        return hsh
     end
 end
 
@@ -64,7 +82,13 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-
+        res = []
+        (0...self.length).each do |outer_i|
+            (0..outer_i).each do |inner_i|
+                res << self[inner_i..outer_i] if length ? outer_i - inner_i == length-1 : true
+            end
+        end
+        return res
     end
 
 
@@ -78,6 +102,12 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
-
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        res = ""
+        self.each_char.with_index do |char, i|
+            alpha_ind = alphabet.index(char)
+            res += alphabet[(alpha_ind + num)%26]
+        end
+        return res
     end
 end
